ce9ec965ed0832e3cdae32f297eb50a9933f94a6
Dududuarte <duartemcg@gmail.com>
2021-12-14 19:06:48 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-function foobar(n) {
+function FooBar(n) {
     for (i = 1; i <= n; i++) {
         let answer = ((i % 3 ? '' : 'Foo') + (i % 5 ? '' : 'Bar'))
         console.log(answer || i)
     }
 }
   
- foobar(15)
+ FooBar(15)
